c30992697883c79308edd90b8850d97d0801f839
Kateryna Tsygankova <tsyg.kata@gmail.com>
2020-10-01 15:27:12 +0300

    script2 added

---
+++ script2.js
@@ -0,0 +1,8 @@
+//скрипт, который выводит строку в формате гость Катерина Цыганкова поселяется в VIP номере 22 
+
+const firstName = "Kateryna";
+const lastName = "Tsygankova";
+const roomType = "VIP";
+const roomNumber = "206";
+
+console.log ("Гость " + firstName + " " + lastName + " поселяется в " + roomType + " номере " + roomNumber);
