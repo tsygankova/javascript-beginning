eb373c3a1d0920d14da1e3fae0225a344140b14a
Kateryna Tsygankova <tsyg.kata@gmail.com>
2020-10-04 18:02:44 +0300

    Update script11.js

--- script11.js
+++ script11.js
@@ -4,7 +4,7 @@
 общую сумму зарлпат totalSalary нужно вывести в консоль*/
 
 // заявить переменные
-const employees = 3;
+const employees = 152;
 const salaryMin = 500;
 const salaryMax = 5000;
 let totalSalary = 0;
