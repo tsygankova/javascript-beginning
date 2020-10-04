03ba24019d1fa7414b4ec0a3accb16658a203d89
Kateryna Tsygankova <tsyg.kata@gmail.com>
2020-10-04 18:54:13 +0300

    Update script12.js

--- script12.js
+++ script12.js
@@ -5,15 +5,31 @@ const min = 2;
 const max = 10;
 let sum = 0;
 
-// цикл
-for ( let i = min; i <= max; i+= 1){
-    console.log (i);
+// // цикл
+// for ( let i = min; i <= max; i+= 1){
+//     console.log (i);
 
-    if ( i % 2 === 0){
-        console.log ('четное', i);
-        sum +=i
+//     if ( i % 2 === 0){
+//         console.log ('четное', i);
+//         sum +=i
+//     }
+// }
+
+// // выдать сумму
+// console.log ('общая сумма четных', sum);
+
+//вариант 2
+
+for ( let i = min; i <= max; i+= 1){
+    if ( i % 2 !== 0){
+        console.log ('нечетное', i);
+        continue;
+    } else {
+        sum +=i;
     }
 }
 
-// выдать сумму
 console.log ('общая сумма четных', sum);
+
+
+
