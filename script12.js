b16d3a92156f7ef00605ed0b62131fd5c2baf0db
Kateryna Tsygankova <tsyg.kata@gmail.com>
2020-10-10 21:28:25 +0300

    smth

--- script12.js
+++ script12.js
@@ -5,18 +5,18 @@ const min = 2;
 const max = 10;
 let sum = 0;
 
-// // цикл
-// for ( let i = min; i <= max; i+= 1){
-//     console.log (i);
+// цикл
+for ( let i = min; i <= max; i+= 1){
+    console.log (i);
 
-//     if ( i % 2 === 0){
-//         console.log ('четное', i);
-//         sum +=i
-//     }
-// }
+    if ( i % 2 === 0){
+        console.log ('четное', i);
+        sum +=i
+    }
+}
 
-// // выдать сумму
-// console.log ('общая сумма четных', sum);
+// выдать сумму
+console.log ('общая сумма четных', sum);
 
 //вариант 2
 
